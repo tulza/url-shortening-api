b5e604ad34ccbd910a8bccd5c4343c325a872395
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-18 20:57:59 +1300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./style/styles.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center align-middle">
+        <h1> thisa template</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
