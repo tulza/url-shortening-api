e68b9eb8f002b0fb6a15ede14824659a9dcb7af6
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-21 22:57:51 +1300

    catches api error and modify ui

--- src/component/ShortenItBar.jsx
+++ src/component/ShortenItBar.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-const LinkBox = ({ original, newLink }) => {
+const NewLinkBox = ({ original, newLink }) => {
   return (
     <div className="flex justify-between rounded-[16px] items-center py-4 px-6 translate-y-[-75px] mt-4 bg-white">
-      <span>{original}</span>
-      <span className="text-sing-up">
+      <a href={original}>{original}</a>
+      <a href={newLink} className="text-sing-up">
         {newLink}
-        <button className="ml-4 py-2 px-8 rounded-[8px] text-white bg-sing-up hover:bg-sing-up-hover">
+        <button className="ml-4 py-2 px-8 rounded-[8px] text-white bg-sing-up hover:bg-sing-up-hover ">
           Copy
         </button>
-      </span>
+      </a>
     </div>
   );
 };
@@ -18,6 +18,7 @@ const LinkBox = ({ original, newLink }) => {
 const ShortenItBar = () => {
   const [inputLink, setLink] = useState("https://www.youtube.com/");
   const [urlBlock, setUrlBlock] = useState(new Array());
+  const [isFetching, setIsFetching] = useState(false);
 
   // handleAPI
   const handleShortenAPI = (link) => {
@@ -28,13 +29,20 @@ const ShortenItBar = () => {
       },
       body: JSON.stringify({ url: link }),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          return Promise.reject();
+        }
+        return response.json();
+      })
+      .then((json) => json.result_url)
       .then((data) => {
-        console.log(data.result_url);
-        setUrlBlock(
-          urlBlock.concat([{ original: link, new: data.result_url }])
-        );
-        console.log(urlBlock);
+        setIsFetching(false);
+        setUrlBlock([{ original: link, new: data }].concat(urlBlock));
+      })
+      .catch((err) => {
+        setIsFetching(false);
+        console.log("invalid link :(");
       });
   };
 
@@ -49,19 +57,30 @@ const ShortenItBar = () => {
           type="input"
           className="z-10 h-[60px] w-[100%] ml-10 pl-4 rounded-[16px]"
           placeholder="Shorten a link here..."
+          value={inputLink}
+          onChange={(e) => {
+            setLink(e.target.value);
+          }}
         />
         <input
           type="button"
-          className="rounded-[16px] font-bold py-2 px-6 text-white bg-sing-up hover:bg-sing-up-hover z-10 mr-10 h-[60px] rounded-[16px]"
-          value="Shorten It!"
+          className="rounded-[16px] font-bold py-2 px-6 text-white bg-sing-up hover:bg-sing-up-hover z-10 mr-10 h-[60px] cursor-pointer"
+          value={isFetching ? "Fetching" : "Shorten It!"}
           onClick={() => {
-            handleShortenAPI(inputLink);
+            if (isFetching == false) {
+              setIsFetching(true);
+              handleShortenAPI(inputLink);
+            }
           }}
         />
       </div>
       {urlBlock.map((url) => {
         return (
-          <LinkBox original={url.original} newLink={url.new} key={uuidv4()} />
+          <NewLinkBox
+            original={url.original}
+            newLink={url.new}
+            key={uuidv4()}
+          />
         );
       })}
     </>
