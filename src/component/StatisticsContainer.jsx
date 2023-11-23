34f38f718df60b39e5af5aadc2a5a8b379c50c0b
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-24 07:46:36 +1300

    mobile statistic

--- src/component/StatisticsContainer.jsx
+++ src/component/StatisticsContainer.jsx
@@ -5,8 +5,8 @@ import customizableIcon from "/images/icon-fully-customizable.svg";
 
 const AspectBox = ({ aspectLabel, details, imgUrl }) => {
   return (
-    <div className="Aspect-box bg-white px-6 pb-4 rounded-lg ">
-      <div className="flex items-center justify-center bg-stats-logo w-[80px] h-[80px] rounded-full translate-y-[-50%] ml-2">
+    <div className="Aspect-box bg-white px-6 pb-4 rounded-lg px-auto flex flex-col mob:items-center">
+      <div className="flex items-center justify-center bg-stats-logo w-[80px] h-[80px] rounded-full translate-y-[-50%] desktop:ml-2">
         <img src={imgUrl} className="w-[40px] h-[40px]"></img>
       </div>
 
@@ -19,18 +19,19 @@ const AspectBox = ({ aspectLabel, details, imgUrl }) => {
 const StatisticContainer = () => {
   return (
     <>
-      <div className="flex flex-col text-center py-16">
-        <p className="font-bold text-[48px] text-large-text">
+      <div className="flex flex-col text-center desktop:py-16">
+        <p className="font-bold text-[48px] text-large-text mob:text-[32px]">
           Advanced Statistics
         </p>
-        <p className="text-[16px]">
+        <p className="text-[16px] text-Grayish-Violet">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
       </div>
       <div>
-        <div className="flex relative items-center justify-center py-16 pb-32 ">
-          <div className="bg-grid-line h-[8px] w-[75%] absolute" />
+        <div className="flex relative items-center justify-center py-16 pb-32 mob:pb-24 ">
+          <div className="desktop-only bg-grid-line h-[8px] w-[75%] absolute" />
+          <div className="mobile-only bg-grid-line h-[75%] w-[8px] absolute" />
           <div id="statistic-grid" className="z-10">
             <AspectBox
               imgUrl={recognitionIcon}
